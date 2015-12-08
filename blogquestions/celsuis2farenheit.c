378049939db3d5109cc3c16aa0f3fefeb02bc9dd
tpof314 <tpof314@126.com>
2015-12-08 04:47:35 +0000

    update

--- blogquestions/celsuis2farenheit.c
+++ blogquestions/celsuis2farenheit.c
@@ -6,7 +6,7 @@ int main() {
     printf("请输入华氏温度：");
     scanf("%lf", &far);
     cel = (far - 32) * 5 / 9;
-    printf("%.3lf 华氏度 = %.3lf 摄氏度\n", far, cel);
+    printf("%.3f 华氏度 = %.3f 摄氏度\n", far, cel);
     
     return 0;
 }
