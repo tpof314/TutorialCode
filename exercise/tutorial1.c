044deb8ba4fef7467e66776b2ae69ee0057e9cda
tpof314 <tpof314@126.com>
2015-12-23 00:35:49 +0000

    add tutorial1

---
+++ exercise/tutorial1.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#define  MAX 100
+
+int data[MAX];	// 一个用来保存数字的数组.
+int count = 0;	// 这个变量用来统计数组中一共添加了几个数字.
+
+// 使用上面给你的两个全局变量，完成下面的所有函数.
+
+/**
+ * 将一个数字value插入到data数组的最后一位.
+ * 比方说，如果data = {5, 3, 2, 1},
+ * 那么, addToEnd(8)的结果是: data = {5, 3, 2, 1, 8}
+ */
+void addToEnd(int value) {
+    // 在这里写代码
+}
+
+/**
+ * 将一个数字value插入到data数组的首位.
+ * 比方说，如果data = {5, 3, 2, 1},
+ * 那么, addToFront(8)的结果是: data = {8, 5, 3, 2, 1}
+ */
+void addToFront(int value) {
+    // 在这里写代码
+}
+
+/**
+ * 删除掉data数组中，下标为k的那个数字.
+ */
+void removeIndex(int k){
+    // 在这里写代码
+}
+/**
+ * 将data数组中所有的value值都删除掉.
+ * 比方说，如果data = {1, 4, 3, 2, 6, 3, 2, 1}，
+ * 那么，removeValue(3)的结果是data = {1, 4, 2, 6, 2, 1}
+ */
+void removeValues(int value) {
+    // 在这里写代码
+}
+
+/**
+ * 统计一下data数组中，value这个数字出现了多少次.
+ */
+int countValue(int value) {
+    // 在这里写代码
+}
+
+/**
+ * 计算data数组的平均值.
+ */
+float getAverage() {
+    // 在这里写代码
+}
+
+/**
+ * 计算data数组的众数.
+ * 众数就是data数组中出现次数最多的那个数字.
+ * 如果众数有多个的话，输出其中任意一个就可以了.
+ */
+int getMode() {
+    // 在这里写代码
+}
+
+/**
+ * 找出data数组中前n个数字的最大值.
+ * 比方说, 如果data = {3, 2, 1, 8, 3, 2, 9}, 那么
+ * getMax(5) = 8, 因为前5个数字中，最大的数字是8
+ */
+int getMax(int n) {
+    // 在这里写代码
+}
+
+/**
+ * 找出data数组中前n个数字中最大值的位置.
+ * 比方说, 如果data = {3, 2, 1, 8, 3, 2, 9}, 那么
+ * getMaxIndex(5) = 3, 因为前5个数字中，最大值8的位置是3
+ */
+int getMaxIndex(int n) {
+    // 在这里写代码
+}
+
+/**
+ * 对data数组进行选择排序.
+ * 按照从小到大的顺序排序.
+ */
+void selectionSort() {
+    // 在这里写代码
+}
+
+/**
+ * 把data数组中所有数字前后倒置.
+ */
+void reverse() {
+    // 在这里写代码
+}
+
+/**
+ * 将data数组中的所有数字按从大到小的顺序排列.
+ * 用两行代码完成.
+ */
+void sortDesc() {
+    // 在这里写代码
+}
+
+/**
+ * 显示所有已经插入到data数组中的数据.
+ */
+void showData() {
+    // 在这里写代码
+}
+
+int main() {
+    return 0;
+}
